0d85827e554ae123a094d44f787735373446f016
kinoshita0623 <kinoshita062323@gmail.com>
2019-04-08 02:16:04 +0900

    投稿をキャプチャするための前準備としてエンティティを準備

--- app/src/main/java/org/panta/misskey_for_android_v2/adapter/StreamingProperty.kt
+++ app/src/main/java/org/panta/misskey_for_android_v2/adapter/StreamingProperty.kt
@@ -1,17 +1,19 @@
 package org.panta.misskey_for_android_v2.adapter
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class StreamingProperty(
+
+@JsonIgnoreProperties(ignoreUnknown = true) data class StreamingProperty(
     @JsonProperty("type") val type: String,
     @JsonProperty("body") val body: Body
 ){
-    data class Body(
+    @JsonIgnoreProperties(ignoreUnknown = true) data class Body(
         @JsonProperty("id") val id: String,
         @JsonProperty("type") val type: String? = null,
         @JsonProperty("body") val body: Body
     ){
-        data class Body(
+        @JsonIgnoreProperties(ignoreUnknown = true) data class Body(
             @JsonProperty("reaction") val reaction: String?,
             @JsonProperty("userId") val userId: String?,
             @JsonProperty("deletedAt") val deletedAt: String?
