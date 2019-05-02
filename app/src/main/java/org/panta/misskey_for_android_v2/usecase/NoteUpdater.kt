597dd407525c96b9b3f5faa4d3c8c94261dc2c9e
kinoshita <kinoshita@kinoshita.xxx>
2019-05-03 07:09:29 +0900

    ノートの情報を追加、更新するNoteUpdaterクラスを追加

---
+++ app/src/main/java/org/panta/misskey_for_android_v2/usecase/NoteUpdater.kt
@@ -0,0 +1,41 @@
+package org.panta.misskey_for_android_v2.usecase
+
+import org.panta.misskey_for_android_v2.view_data.NoteViewData
+
+class NoteUpdater{
+
+    private val noteAdjustment = NoteAdjustment(false)
+
+    fun addReaction(reaction: String, viewData: NoteViewData, hasMyReaction: Boolean): NoteViewData{
+        val note = viewData.note
+        val updatedReactionCounts = updateUpdateReactionCounts(reaction, note.reactionCounts)
+
+        val updatedNote = if(hasMyReaction){
+            note.copy(myReaction = reaction, reactionCounts = updatedReactionCounts)
+        }else{
+            note.copy(reactionCounts = updatedReactionCounts)
+        }
+
+
+        val reactionCountPair = noteAdjustment.createReactionCountPair(updatedReactionCounts)
+        return viewData.copy(note = updatedNote, reactionCountPairList = reactionCountPair)
+
+    }
+
+    private fun updateUpdateReactionCounts(reaction: String, map: Map<String, Int>?): Map<String, Int>{
+        return if(map != null && map.isNotEmpty()){
+            val hashMap = HashMap<String, Int>(map)
+            val hasReaction = hashMap[reaction]
+            if(hasReaction != null){
+                hashMap[reaction] = hasReaction + 1
+            }else{
+                hashMap[reaction] = 1
+            }
+            hashMap
+        }else{
+            hashMapOf(reaction to 1)
+        }
+    }
+
+
+}
